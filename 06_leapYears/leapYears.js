699bdc1c27efb858052a76168ea67ec06377b53b
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-07-26 17:51:17 -0500

    complete!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    const answer = ((year % 4 == 0) && !(year % 100 == 0)) ? true : (year % 400 == 0) ? true : false;
+    return answer;
 };
 
 // Do not edit below this line
