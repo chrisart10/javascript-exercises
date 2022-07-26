e0122cc2bbdcfb397d4bbf80167899ff78284adb
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-07-26 18:27:51 -0500

    complete!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fvalue) {
+  return  Math.round(((fvalue - 32) / 1.8)*10)/10 ;
 };
 
-const ctof = function() {
-
+const ctof = function(cvalue) {
+  return Math.round((cvalue * 1.8 + 32)*10)/10 ;
 };
 
 // Do not edit below this line
