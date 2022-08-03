e788c8406e2f7f57d76352372ad526f7375263f8
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-08-03 12:52:15 -0500

    complete!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    if (string != '') {
 
+        string = string.toLowerCase();
+        const regex = /[a-z]/g;
+        const array = string.match(regex);
+        const length = array.length;
+        const middle = Math.floor(length / 2);
+        
+        for (let index in array) {
+            if (index == middle + 1) return true;
+            if (array[index] != array[length - index - 1]) return false;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
