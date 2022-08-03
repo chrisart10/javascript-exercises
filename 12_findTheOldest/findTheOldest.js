184b87c790f012584748106a4f12c22d1351e610
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-08-03 18:45:32 -0500

    compelete!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
 
+    const oldest = array.sort((previus, current) => {
+
+        if (!previus.yearOfDeath) previus.yearofDeath = Number((new Date()).getFullYear());
+        if (!current.yearOfDeath) current.yearOfDeath = Number((new Date()).getFullYear());
+        
+        const previusRange = previus.yearOfDeath - previus.yearOfBirth;
+        const currentRange = current.yearOfDeath - current.yearOfBirth;
+        
+        return previusRange > currentRange ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
