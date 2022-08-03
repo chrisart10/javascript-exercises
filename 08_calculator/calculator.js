a3d09e145a418766a1bc93c79b46baafc99dc587
Christian Espinoza <christian.espinoza1008@gmail.com>
2022-08-02 21:55:22 -0500

    complete!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (operand1, operand2) {
+  return operand1 + operand2;
 };
 
-const subtract = function() {
-	
+const subtract = function (operand1, operand2) {
+  return operand1 - operand2;
 };
 
-const sum = function() {
-	
+const sum = function ([...operands]) {
+  let total = 0;
+  for (let index = 0; index < operands.length; index++) {
+    total += operands[index];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function ([...operands]) {
+  let total = operands[0];
+  for (let index = 1; index < operands.length; index++) {
+    total *= operands[index];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (operand1, operand2) {
+  return operand1 ** operand2;
 };
 
-const factorial = function() {
-	
+const factorial = function (operand) {
+  if (operand == 0) return 1;
+  let total = 1;
+  for (let index = 2; index < operand + 1; index++) {
+    total *= index;
+  }
+  return total;
 };
 
 // Do not edit below this line
